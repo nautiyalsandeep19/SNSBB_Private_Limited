d39027be65c4ae3e8a65ff569cc1ac1158da8edb
shubham-hubgit <raturi17147714@gmail.com>
2026-02-14 13:22:21 +0530

    shubham updated code

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,21 @@
-import client1 from "./client1.jpeg";
+import client1 from "./Client1.png";
+import client2 from "./Client2.jpg";
+import client3 from "./Client3.png";
+import client4 from "./Client4.jpg";
+import client5 from "./Client5.png";
+import client6 from "./Client6.png";
+import client7 from "./Client7.png";
+import client8 from "./Client8.png";
+// import client9 from "./Client9.png";
 
 export const assets = {
-  client1,
+  client1, 
+  client2,
+  client3,
+  client4,
+  client5,
+  client6,
+  client7,
+  client8,
+  // client9
 };
